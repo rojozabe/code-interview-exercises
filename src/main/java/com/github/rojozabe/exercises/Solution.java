ac3e9a5e361cc33927b6a20a3b96c256c50028f0
Ronel José Zapata Beltrán <rojozabe@gmail.com>
2019-01-14 18:06:57 -0600

    first example

---
+++ src/main/java/com/github/rojozabe/exercises/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author rzapata
+ * This is the point of entry to run all the code
+ *
+ */
+public class Solution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
