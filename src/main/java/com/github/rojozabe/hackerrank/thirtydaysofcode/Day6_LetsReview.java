b2181e9bef066aebe2fe6d295e4b07053eaf067c
Ronel José Zapata Beltrán <rojozabe@gmail.com>
2019-02-25 14:56:41 -0600

    solved HackerRank day 6 of code

---
+++ src/main/java/com/github/rojozabe/hackerrank/thirtydaysofcode/Day6_LetsReview.java
@@ -0,0 +1,27 @@
+package hackerrank.thirtydaysofcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import utils.FileHelper;
+
+public class Day6_LetsReview {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = FileHelper.readFile("./src/test/java/com/github/rojozabe/hackerrank/thirtydaysofcode/Day6_LetsReview.txt");
+        int t = Integer.parseInt(br.readLine().trim());
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine().trim();
+            //evens
+            for (int j = 0; j < s.length(); j++) {
+                if (j % 2 == 0) System.out.printf("%c", s.charAt(j));                
+            }
+            System.out.print(" ");
+            //odds
+            for (int j = 0; j < s.length(); j++) {
+                if ((j + 1) % 2 == 0) System.out.printf("%c", s.charAt(j));                
+            }
+            System.out.println();
+        }
+        br.close();
+    }
+}
