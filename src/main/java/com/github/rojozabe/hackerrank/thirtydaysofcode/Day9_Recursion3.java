4e94ede97aa603780e0d3667ff0df192ba2d05d9
Ronel José Zapata Beltrán <rojozabe@gmail.com>
2019-02-27 11:04:56 -0600

    solved HackerRank day 9 of code

---
+++ src/main/java/com/github/rojozabe/hackerrank/thirtydaysofcode/Day9_Recursion3.java
@@ -0,0 +1,20 @@
+package hackerrank.thirtydaysofcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import utils.FileHelper;
+
+public class Day9_Recursion3 {
+    private static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+    
+    public static void main(String[] main) throws IOException {
+        BufferedReader br = FileHelper.readFile("./src/test/java/com/github/rojozabe/hackerrank/thirtydaysofcode/Day9_Recursion3.txt");
+        int n = Integer.parseInt(br.readLine().trim());
+        System.out.println(factorial(n));
+        br.close();
+    }
+}
